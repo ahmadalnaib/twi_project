9dc06cf304e0fd7c79d95afaba59eddb1c0225f6
Ahmed Habeeb <ahmedhabeeb@Ahmeds-Mac-mini.fritz.box>
2021-05-24 19:28:22 +0200

    add content index

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
 $title="Home";
+require_once("template/header.php"); ?>
 
-?>
 
+<div class="grid">
+  <div class="user-info">
+    <div>
+      <img src="<?php echo $user['profile_pic'] ?>" alt="">
+    </div>
+    <div>
+      <span><?php echo $user['first_name']?></span>
+      <span>posts  <?php echo $user['num_posts'] ?></span>
+      <span>likes  <?php echo $user['num_likes'] ?></span>
+    </div>
+  </div>
+  <div class="main">
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, temporibus.</p>
+  </div>
 
-<?php require_once("template/header.php"); ?>
+</div>
 
 
 
